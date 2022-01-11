bf08ea41ae3ee8b6e11b6ec724bcb1ecb8872168
Dave Ch <dabbidchavarria@gmail.com>
2022-01-10 21:54:15 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Platform, StatusBar } from 'react-native';
 
+/* SafeAreaView keeps the app inside te correct boundaries. Only for IOS version 11*/
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  searchContainer: {
+    backgroundColor: 'red',
+    flex: 0.1,
+  },
+  contentContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
