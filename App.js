18455b04da60739a3400181b71a54f280331c6f7
Dave Ch <dabbidchavarria@gmail.com>
2022-01-17 21:49:01 -0600

    Start commit

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView, Platform, StatusBar } from 'react-native';
+
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import { RestaurantsScreen } from './src/screens/restaurants.screen';
 
 /* SafeAreaView keeps the app inside te correct boundaries. Only for IOS version 11*/
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-      <View style={styles.contentContainer}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    </SafeAreaView>
+    <PaperProvider>
+      <RestaurantsScreen />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  searchContainer: {
-    backgroundColor: 'red',
-    flex: 0.1,
-  },
-  contentContainer: {
-    backgroundColor: 'blue',
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
---
+++ src/screens/restaurants.screen.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+import { Searchbar } from 'react-native-paper';
+import styled from 'styled-components/native';
+
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+
+/*
+if StatusBar.currentHeight exists, returns `margin-top: ${StatusBar.currentHeight}px`}
+In IOS SafeAreaView protects us from this
+*/
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar placeholder="Search" />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
